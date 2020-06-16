45ff782395b0200ad8a992a2e1139ab517886982
angelodarcy <angelomolin@gmail.com>
2020-06-16 16:57:53 -0400

    Criação do Projeto

---
+++ src/main/java/com/exemploangelo/exemploangelo/ExemploangeloApplication.java
@@ -0,0 +1,13 @@
+package com.exemploangelo.exemploangelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploangeloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExemploangeloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exemploangelo/exemploangelo/ExemploangeloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemploangelo.exemploangelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploangeloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
